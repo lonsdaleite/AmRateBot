02ab83245de617a73872d7b4d7104b9318a3ea57
Alexander Dulyasov <streamserfer@gmail.com>
2022-10-14 13:37:13 +0400

    mir fix

--- mir.py
+++ mir.py
@@ -2,12 +2,16 @@ import random
 
 from bs4 import BeautifulSoup
 import requests
+from urllib.request import Request, urlopen
 import time
 from rate import add_rate
 
 
 def add_mir(url="https://mironline.ru/support/list/kursy_mir/", all_rates=None):
-    page = requests.get(url)
+    # page = requests.get(url)
+    hdr = {'User-Agent': 'Mozilla/5.0'}
+    req = Request(url, headers=hdr)
+    page = urlopen(req)
     # print(page.status_code)
     result = None
     try_num = 0
@@ -19,18 +23,16 @@ def add_mir(url="https://mironline.ru/support/list/kursy_mir/", all_rates=None):
         if try_num > 5:
             print("ERROR: Can not get MIR rate")
             break
-        if page.status_code == 200:
-            soup = BeautifulSoup(page.text, "lxml")
-            # print(soup)
-
-            rates = soup.find_all("tr")
-            for rate in rates:
-                currency = rate.findNext("p")
-                if currency.text.strip() == "Армянский драм":
-                    value = currency.findNext("p")
-                    result = float(value.text.strip().replace(",", "."))
-                    add_rate(all_rates, "rur", "tinkoff", "amd", "cash", "atm", result, "to")
-                    break
-        else:
-            print("Warn: MIR status code: " + str(page.status_code))
+        soup = BeautifulSoup(page, "lxml")
+        # print(soup)
+        rates = soup.find_all("tr")
+        if len(rates) == 0:
             continue
+
+        for rate in rates:
+            currency = rate.findNext("p")
+            if currency.text.strip() == "Армянский драм":
+                value = currency.findNext("p")
+                result = float(value.text.strip().replace(",", "."))
+                add_rate(all_rates, "rur", "tinkoff", "amd", "cash", "atm", result, "to")
+                break
