448a9d5b459f9179d426292ebe1b1a8deb0d9cbd
Alexander Dulyasov <streamserfer@gmail.com>
2023-02-27 17:03:50 +0400

    raif swift added; tinkoff eur swift removed

--- add_rates.py
+++ add_rates.py
@@ -1,6 +1,6 @@
 from rate import add_rate
 from rate_sources.id_pay import add_id_pay
-# from rate_sources.raiffeisen_broker import add_raiffeisen_broker
+from rate_sources.raiffeisen_broker import add_raiffeisen_broker
 from rate_sources.rate_am import add_rate_am
 from rate_sources.sas import add_sas
 from rate_sources.mir import add_mir
@@ -16,9 +16,9 @@ def add_const_rates(all_rates=None):
     add_rate(all_rates, "rur", "bank", "ru", "",           "rur", "bank", "ru", "",         "transfer", 1,     "from")
     add_rate(all_rates, "rur", "bank", "ru", "",           "rur", "bank", "am", "",         "transfer", 1,     "from")
     add_rate(all_rates, "usd", "bank", "ru", "tinkoff",    "usd", "bank", "am", "",         "swift",    1.01,  "from")
-    add_rate(all_rates, "eur", "bank", "ru", "tinkoff",    "eur", "bank", "am", "",         "swift",    1.01,  "from")
-    add_rate(all_rates, "usd", "bank", "ru", "raif",       "usd", "bank", "am", "",         "swift",    1.03,  "from")
-    add_rate(all_rates, "eur", "bank", "ru", "raif",       "eur", "bank", "am", "",         "swift",    1.03,  "from")
+    # add_rate(all_rates, "eur", "bank", "ru", "tinkoff",    "eur", "bank", "am", "",         "swift",    1.01,  "from")
+    add_rate(all_rates, "usd", "bank", "ru", "raif",       "usd", "bank", "am", "",         "swift",    1.02,  "from")
+    add_rate(all_rates, "eur", "bank", "ru", "raif",       "eur", "bank", "am", "",         "swift",    1.02,  "from")
     add_rate(all_rates, "rur", "bank", "am", "yunibank",   "rur", "cash", "am", "",         "bank",     1.02,  "from")
     add_rate(all_rates, "usd", "bank", "am", "yunibank",   "usd", "cash", "am", "",         "bank",     1.02,  "from")
     add_rate(all_rates, "eur", "bank", "am", "yunibank",   "eur", "cash", "am", "",         "bank",     1.02,  "from")
@@ -40,7 +40,7 @@ def add_all_rates(all_rates=None):
     add_sas(all_rates=all_rates)
     add_mir(all_rates=all_rates)
     add_tinkoff_broker(all_rates=all_rates)
-    # add_raiffeisen_broker(all_rates=all_rates)
+    add_raiffeisen_broker(all_rates=all_rates)
     add_unistream(all_rates=all_rates)
     add_rate_am(convert_type="cash", all_rates=all_rates)
     add_rate_am(convert_type="non-cash", all_rates=all_rates)
