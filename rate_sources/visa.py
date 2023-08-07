04edceea88156dd0b61f8ae8e0d572dfa8ea039e
Alexander Dulyasov <streamserfer@gmail.com>
2023-08-07 10:01:02 +0200

    visa ardshin fix

--- rate_sources/visa.py
+++ rate_sources/visa.py
@@ -32,16 +32,16 @@ def add_day(dt_str, days):
 
 
 def get_visa_rate(url="https://usa.visa.com/cmsapi/fx/rates?amount=1", from_curr="AMD", to_curr="RSD",
-                  convert_fee=0.5):
+                  convert_fee_prc=0.5):
     dt = datetime.datetime.now().strftime("%m%%2F%d%%2F%Y")
     dt_min = add_day(dt, -10)
-    full_rate = get_json_rate(dt, url, from_curr, to_curr, convert_fee)
+    full_rate = get_json_rate(dt, url, from_curr, to_curr, convert_fee_prc)
     # print(full_rate)
 
     while full_rate is None and dt > dt_min:
         dt = add_day(dt, -1)
         time.sleep(0.5)
-        full_rate = get_json_rate(dt, url, from_curr, to_curr, convert_fee)
+        full_rate = get_json_rate(dt, url, from_curr, to_curr, convert_fee_prc)
 
     rate_with_fee = None
     if full_rate is not None:
@@ -53,13 +53,13 @@ def get_visa_rate(url="https://usa.visa.com/cmsapi/fx/rates?amount=1", from_curr
 
 def add_visa(all_rates=None):
     # ArdshinBank
-    ardshin_convert_fee = 0.5
+    ardshin_convert_fee_prc = 0
     ardshin_cash_fee = 0.015
     ardshin_c2c_fee = 0.005
 
-    ardshin_amd_rsd_rate = get_visa_rate(convert_fee=ardshin_convert_fee, from_curr="AMD", to_curr="RSD")
-    ardshin_eur_rsd_rate = get_visa_rate(convert_fee=ardshin_convert_fee, from_curr="EUR", to_curr="RSD")
-    ardshin_amd_eur_rate = get_visa_rate(convert_fee=ardshin_convert_fee, from_curr="AMD", to_curr="EUR")
+    ardshin_amd_rsd_rate = get_visa_rate(convert_fee_prc=ardshin_convert_fee_prc, from_curr="AMD", to_curr="RSD")
+    ardshin_eur_rsd_rate = get_visa_rate(convert_fee_prc=ardshin_convert_fee_prc, from_curr="EUR", to_curr="RSD")
+    ardshin_amd_eur_rate = get_visa_rate(convert_fee_prc=ardshin_convert_fee_prc, from_curr="AMD", to_curr="EUR")
 
     add_rate(all_rates, "amd", "bank", "am", "ardshinbank", "rsd", "pos", "rs", "", "pos", ardshin_amd_rsd_rate, "from")
     add_rate(all_rates, "eur", "bank", "am", "ardshinbank", "rsd", "pos", "rs", "", "pos", ardshin_eur_rsd_rate, "from")
@@ -73,11 +73,11 @@ def add_visa(all_rates=None):
              ardshin_amd_eur_rate * (1 + ardshin_c2c_fee), "from")
 
     # Unibank
-    unibank_convert_fee = 2
+    unibank_convert_fee_prc = 2
     unibank_cash_fee = 0.013
 
-    unibank_amd_rsd_rate = get_visa_rate(convert_fee=unibank_convert_fee, from_curr="AMD", to_curr="RSD")
-    unibank_usd_rsd_rate = get_visa_rate(convert_fee=unibank_convert_fee, from_curr="USD", to_curr="RSD")
+    unibank_amd_rsd_rate = get_visa_rate(convert_fee_prc=unibank_convert_fee_prc, from_curr="AMD", to_curr="RSD")
+    unibank_usd_rsd_rate = get_visa_rate(convert_fee_prc=unibank_convert_fee_prc, from_curr="USD", to_curr="RSD")
 
     add_rate(all_rates, "amd", "bank", "am", "yunibank", "rsd", "pos", "rs", "", "pos", unibank_amd_rsd_rate, "from")
     add_rate(all_rates, "usd", "bank", "am", "yunibank", "rsd", "pos", "rs", "", "pos", unibank_usd_rsd_rate, "from")
